cc7e51b481c5eef2be101275ad86b751c0a2b426
James Dempsey <jamespd@gmail.com>
2024-10-07 22:10:27 +1300

    Tidy up.

--- main.go
+++ main.go
@@ -21,32 +21,11 @@ const (
 
 // Style definitions.
 var (
-
 	// General.
 
 	subtle    = lipgloss.AdaptiveColor{Light: "#D9DCCF", Dark: "#383838"}
 	highlight = lipgloss.AdaptiveColor{Light: "#874BFD", Dark: "#7D56F4"}
 	special   = lipgloss.AdaptiveColor{Light: "#43BF6D", Dark: "#73F59F"}
-
-	divider = lipgloss.NewStyle().
-		SetString("•").
-		Padding(0, 1).
-		Foreground(subtle).
-		String()
-
-	url = lipgloss.NewStyle().Foreground(special).Render
-
-	// Title.
-
-	titleStyle = lipgloss.NewStyle().
-			MarginLeft(1).
-			MarginRight(5).
-			Padding(0, 1).
-			Italic(true).
-			Foreground(lipgloss.Color("#FFF7DB")).
-			SetString("Lip Gloss")
-
-	descStyle = lipgloss.NewStyle().MarginTop(1)
 
 	// Dialog.
 
@@ -59,49 +38,12 @@ var (
 			BorderRight(true).
 			BorderBottom(true)
 
-	buttonStyle = lipgloss.NewStyle().
-			Foreground(lipgloss.Color("#FFF7DB")).
-			Background(lipgloss.Color("#888B7E")).
-			Padding(0, 3).
-			MarginTop(1)
-
-	activeButtonStyle = buttonStyle.
-				Foreground(lipgloss.Color("#FFF7DB")).
-				Background(lipgloss.Color("#F25D94")).
-				MarginRight(2).
-				Underline(true)
-
-
-	// Status Bar.
-
-	statusNugget = lipgloss.NewStyle().
-			Foreground(lipgloss.Color("#FFFDF5")).
-			Padding(0, 1)
-
-	statusBarStyle = lipgloss.NewStyle().
-			Foreground(lipgloss.AdaptiveColor{Light: "#343433", Dark: "#C1C6B2"}).
-			Background(lipgloss.AdaptiveColor{Light: "#D9DCCF", Dark: "#353533"})
-
-	statusStyle = lipgloss.NewStyle().
-			Inherit(statusBarStyle).
-			Foreground(lipgloss.Color("#FFFDF5")).
-			Background(lipgloss.Color("#FF5F87")).
-			Padding(0, 1).
-			MarginRight(1)
-
-	encodingStyle = statusNugget.
-			Background(lipgloss.Color("#A550DF")).
-			Align(lipgloss.Right)
-
-	statusText = lipgloss.NewStyle().Inherit(statusBarStyle)
-
-	fishCakeStyle = statusNugget.Background(lipgloss.Color("#6124DF"))
-
 	// Page.
 
 	docStyle = lipgloss.NewStyle().Padding(0, 0, 0, 0)
 )
-var helpStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("#626262")).Render
+
+//var helpStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("#626262")).Render
 
 // main entry point
 func main() {
@@ -197,18 +139,14 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 func (m model) View() string {
 	physicalWidth, _, _ := term.GetSize(int(os.Stdout.Fd()))
 	doc := strings.Builder{}
+
 	// Dialog
 	{
-//	return "\n" +
-////		pad + m.duration + "\n" +
-//		pad + m.taskText + "\n" +
-//		pad + m.progress.View() + "\n\n" +
-		//okButton := activeButtonStyle.Render("Yes")
-		//cancelButton := buttonStyle.Render("Maybe")
         contentWidth := int(math.Floor(float64(physicalWidth-4) * 0.8))
+
 		text := lipgloss.NewStyle().Width(contentWidth).Align(lipgloss.Center).Render(m.taskText)
 		progress := lipgloss.NewStyle().Width(contentWidth).Align(lipgloss.Center).Render(m.progress.View())
-		//buttons := lipgloss.JoinHorizontal(lipgloss.Top, okButton, cancelButton)
+
 		ui := lipgloss.JoinVertical(lipgloss.Center, text, progress)
 
 		dialog := lipgloss.Place(physicalWidth, 9,
@@ -222,20 +160,6 @@ func (m model) View() string {
 		doc.WriteString(dialog )
 	}
 	return docStyle.Render(doc.String())
-//	style := lipgloss.NewStyle().
-//		SetString(m.taskText).
-//		Width(50).
-//		Foreground(lipgloss.Color("63"))
-//	return style.Render()
-
-
-
-//	//pad := strings.Repeat(" ", padding)
-//	return "\n" +
-////		pad + m.duration + "\n" +
-//		pad + m.taskText + "\n" +
-//		pad + m.progress.View() + "\n\n" +
-//		pad + helpStyle("Press any key to quit")
 }
 
 // Tick command.  Called once during init to schedule the first tick and then again when the tick is received in the update function.
@@ -244,9 +168,4 @@ func tickCmd() tea.Cmd {
 		return tickMsg(t)
 	})
 }
-
-const (
-	width = 96
-	columnWidth = 30
-)
 
